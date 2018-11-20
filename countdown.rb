04020802f7642d558d24282c850aa61c19aace75
Tymeckia Kendall <tykend@gmail.com>
2018-11-20 19:13:23 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,9 +1,8 @@
 #write your code here
 
 def countdown(seconds)
-    number = seconds
-      while number > 0
-        puts "#{number} SECOND(S)!"
+      while seconds > 0
+        puts "#{seconds} SECOND(S)!"
         number -= 1
     end
   puts "HAPPY NEW YEAR!"
@@ -11,7 +10,7 @@ end
 
 def countdown_with_sleep(seconds)
         while seconds > 0
-              puts "#{number} SECOND(S)!"
+              puts "#{seconds} SECOND(S)!"
           seconds -= 1
     end
     puts "HAPPY NEW YEAR!"
