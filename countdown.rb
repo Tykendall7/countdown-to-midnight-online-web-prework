034741d68c6b1ac70938c4eae971f9a1337f0dda
Tymeckia Kendall <tykend@gmail.com>
2018-11-20 19:01:26 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,7 +1,7 @@
 #write your code here
 
-def countdown(number)
-    number = 10
+def countdown(seconds)
+    number = seconds
     loop do
       while number > 0
         puts "#{number} SECOND(S)!"
