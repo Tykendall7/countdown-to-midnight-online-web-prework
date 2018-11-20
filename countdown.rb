9afa5045ec2c0626ac5546e59aee3f21e286cac6
Tymeckia Kendall <tykend@gmail.com>
2018-11-20 18:49:57 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,10 @@
 
 def countdown
     PHRASE= ""
-    
+    loop do
+      while number == 0
+        puts "#{number} SECONDS!"
+        x -= 1
     END
-  puts "#{number} SECONDS!"
+  puts "   "
 end
