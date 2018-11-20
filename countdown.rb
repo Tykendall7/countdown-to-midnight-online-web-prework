6e473a8c3b21ea30f3b4d3d2966116ee205ec1e4
Tymeckia Kendall <tykend@gmail.com>
2018-11-20 19:09:03 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,6 @@
 
 def countdown(seconds)
     number = seconds
-    loop do
       while number > 0
         puts "#{number} SECOND(S)!"
         number -= 1
@@ -11,7 +10,6 @@ def countdown(seconds)
 end
 
 def countdown_with_sleep(seconds)
-      loop do 
         while seconds > 0
           seconds -= 1
     end
